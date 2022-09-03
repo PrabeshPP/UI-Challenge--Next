fe30e12b619ae300899cfcd6a49acbba152da396
Prabesh <prabesh.bista369@gmail.com>
2022-09-03 17:46:20 -0400

    blogging s

--- Component/Layout/Header/MainNavigation.tsx
+++ Component/Layout/Header/MainNavigation.tsx
@@ -4,8 +4,10 @@ import classes from "../../../styles/Header.module.scss";
 const MainNavigation = () => {
     return (
         <div className={classes.nav}>
-            <div>Prabesh's Blog</div>
-            <div>
+            <div className={classes.logoBox}>
+                <h1 className={classes.logo}>Prabesh's Blog</h1>
+            </div>
+            <div className={classes.navBox}>
                 <div>Home</div>
                 <div>Recent</div>
                 <div></div>
